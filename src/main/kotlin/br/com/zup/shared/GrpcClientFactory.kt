bf8305ebbf2f4b8e2cce49b99cbc6953320e06fe
Gustavo Santos <gsantoset@gmail.com>
2021-03-10 21:11:19 -0300

    Criado os clients dos 4 serviços grpc existentes que serão inclusos nesse projeto.

---
+++ src/main/kotlin/br/com/zup/shared/GrpcClientFactory.kt
@@ -0,0 +1,35 @@
+package br.com.zup.shared
+
+import br.com.zup.KeymanagerConsultaChavePixGrpcServiceGrpc
+import br.com.zup.KeymanagerListaChavesPixGrpcServiceGrpc
+import br.com.zup.KeymanagerRegistraChavePixGrpcServiceGrpc
+import br.com.zup.KeymanagerRemoveChavePixGrpcServiceGrpc
+import io.grpc.ManagedChannel
+import io.micronaut.context.annotation.Factory
+import io.micronaut.grpc.annotation.GrpcChannel
+import javax.inject.Singleton
+
+@Factory
+class GrpcClientFactory(@GrpcChannel("keymanager") val channel: ManagedChannel) {
+
+    @Singleton
+    fun registrar(): KeymanagerRegistraChavePixGrpcServiceGrpc.KeymanagerRegistraChavePixGrpcServiceBlockingStub? {
+        return KeymanagerRegistraChavePixGrpcServiceGrpc.newBlockingStub(channel)
+    }
+
+    @Singleton
+    fun remover(): KeymanagerRemoveChavePixGrpcServiceGrpc.KeymanagerRemoveChavePixGrpcServiceBlockingStub? {
+        return KeymanagerRemoveChavePixGrpcServiceGrpc.newBlockingStub(channel)
+    }
+
+    @Singleton
+    fun consultar(): KeymanagerConsultaChavePixGrpcServiceGrpc.KeymanagerConsultaChavePixGrpcServiceBlockingStub? {
+        return KeymanagerConsultaChavePixGrpcServiceGrpc.newBlockingStub(channel)
+    }
+
+    @Singleton
+    fun listar(): KeymanagerListaChavesPixGrpcServiceGrpc.KeymanagerListaChavesPixGrpcServiceBlockingStub? {
+        return KeymanagerListaChavesPixGrpcServiceGrpc.newBlockingStub(channel)
+    }
+
+}
