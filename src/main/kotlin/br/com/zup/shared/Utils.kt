fce21017adb6b818611dd2fd41be9e2b7bbb048e
Gustavo Santos <gsantoset@gmail.com>
2021-03-11 10:23:33 -0300

    Melhorias de sintaxe do Kotlin.

--- src/main/kotlin/br/com/zup/shared/Utils.kt
+++ src/main/kotlin/br/com/zup/shared/Utils.kt
@@ -4,10 +4,8 @@ import com.google.protobuf.Timestamp
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
-fun timestampToLocalDateTime(timestamp: Timestamp): LocalDateTime {
-    return LocalDateTime.ofEpochSecond(
-        timestamp.seconds,
-        timestamp.nanos,
-        ZoneOffset.UTC
-    )
-}
+fun timestampToLocalDateTime(timestamp: Timestamp) = LocalDateTime.ofEpochSecond(
+    timestamp.seconds,
+    timestamp.nanos,
+    ZoneOffset.UTC
+)
