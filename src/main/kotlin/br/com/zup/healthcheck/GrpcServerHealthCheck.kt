32fa16835c72e61aba01040d0bcb105dd6cbd1d6
Gustavo Santos <gsantoset@gmail.com>
2021-03-17 11:52:34 -0300

    Healthcheck alterado para fins didáticos.

--- src/main/kotlin/br/com/zup/healthcheck/GrpcServerHealthCheck.kt
+++ src/main/kotlin/br/com/zup/healthcheck/GrpcServerHealthCheck.kt
@@ -4,37 +4,36 @@ import io.grpc.health.v1.HealthCheckRequest
 import io.grpc.health.v1.HealthGrpc
 import io.micronaut.context.annotation.Value
 import io.micronaut.health.HealthStatus
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
 import io.micronaut.http.client.RxHttpClient
 import io.micronaut.management.health.indicator.HealthIndicator
 import io.micronaut.management.health.indicator.HealthResult
-import io.swagger.v3.oas.annotations.Hidden
 import org.reactivestreams.Publisher
 import javax.inject.Singleton
 
 @Singleton
 class GrpcServerHealthCheck(
     private val client: RxHttpClient,
+    private val healthCheckClient: HealthGrpc.HealthBlockingStub
 ) : HealthIndicator {
 
     @Value("\${healthcheck.keymanager.url}")
     private lateinit var uri: String
 
+    /*
+    * Código mantido somente por fins de conhecimento, uma vez que esse healthcheck faz
+    * um check em um sistema externo (keymanager-grpc) e ele está fora do nosso controle,
+    * e também por questões de segurança.
+    */
     override fun getResult(): Publisher<HealthResult> {
-        return client.exchange(uri)
-            .map { HealthResult.builder("Keymanager-grpc", HealthStatus.UP).build() }
-            .onErrorReturn { HealthResult.builder("Keymanager-grpc", HealthStatus.DOWN).build() }
-    }
-}
-
-@Hidden
-@Controller("/check-grpc-status")
-class GrpcHealthCheckController(
-    private val healthCheckClient: HealthGrpc.HealthBlockingStub
-) {
-    @Get
-    fun check() {
-        healthCheckClient.check(HealthCheckRequest.newBuilder().setService("Keymanager").build())
+        return Publisher<HealthResult> {
+            try {
+                healthCheckClient.check(HealthCheckRequest.newBuilder().setService("Keymanager").build())
+                it.onNext(HealthResult.builder("Keymanager-grpc", HealthStatus.UP).build())
+                it.onComplete()
+            } catch (e: Exception) {
+                it.onNext(HealthResult.builder("Keymanager-grpc", HealthStatus.DOWN).build())
+                it.onComplete()
+            }
+        }
     }
 }
